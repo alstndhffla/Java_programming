7d578ad585e970e05b677e80203a8ae921c61dbb
Minsoo <alstndhffla@gmail.com>
2021-04-21 18:59:58 +0900

    update

--- src/generics/GenericPrinter.java
+++ src/generics/GenericPrinter.java
@@ -1,7 +1,8 @@
 package generics;
 
 //파우더와 플라스틱 액체를 재료로 모형을 출력하는 프린터를 제네릭 클래스로 정의
-public class GenericPrinter<T> {
+//extends 에약어로 사용할 수 있는 자료형에 제한을 둠.
+public class GenericPrinter<T extends Material> {
 	private T material;	//T 자료형으로 선언한 변수
 
 	public void setMaterial(T material) {
@@ -14,5 +15,10 @@ public class GenericPrinter<T> {
 	
 	public String toString() {
 		return material.toString();
+	}
+	
+	//이거 왜 오류지???
+	public void printing() {
+		material doPrinting();
 	}
 }
--- src/generics/Powder.java
+++ src/generics/Powder.java
@@ -1,6 +1,7 @@
 package generics;
 
-public class Powder {
+//재료에 제한을 두기 위해 material 을 상속시킴
+public class Powder extends Material {
 	public void doPrinting() {
 		System.out.println("Powder 재료로 출력합니다.");
 	}
